fce432d1b9e88a05afaa1e281b99d844056b48dc
Hirenlimbad <limbadhiren00@gmail.com>
2024-07-15 23:58:46 +0530

    updated trendings

--- src/Dashboard/Trendings/TrendingDash.jsx
+++ src/Dashboard/Trendings/TrendingDash.jsx
@@ -29,56 +29,6 @@ function TrendingDash() {
             <div>
                 <p className='text-capitalize'> {topTrending.quote} </p>
                 <p> - {topTrending.author_name} </p>
-
-
-                <p className='h5'>Top authors </p>
-                <div className="d-flex justify-content-around trending">
-                    <div>
-                        <p> 
-                            
-                        {authorTrend[0] &&
-                        <img
-                                src={`data:image/jpeg;base64,${authorTrend[0].profileImage}`}
-                                onError={(e) => { e.target.onerror = null; e.target.src = 'https://www.pngall.com/wp-content/uploads/5/Profile.png' }}
-                                alt="User Profile Picture"
-                                className="profile-pic"
-                            />
-                        }
-                              {authorTrend[0] && (<> {authorTrend[0].uname} </>)} </p>
-                        <p> 
-                        {authorTrend[0] &&
-                        <img 
-                                src={`data:image/jpeg;base64,${authorTrend[1].profileImage}`}
-                                onError={(e) => { e.target.onerror = null; e.target.src = 'https://www.pngall.com/wp-content/uploads/5/Profile.png' }}
-                                alt="User Profile Picture"
-                                className="profile-pic"
-                            />
-                        }
-                              {authorTrend[1] && (<> {authorTrend[1].uname} </>)} </p>
-                    </div>
-                    <div>
-                        <p> 
-                        {authorTrend[0] &&
-                        <img
-                                src={`data:image/jpeg;base64,${authorTrend[2].profileImage}`}
-                                onError={(e) => { e.target.onerror = null; e.target.src = 'https://www.pngall.com/wp-content/uploads/5/Profile.png' }}
-                                alt="User Profile Picture"
-                                className="profile-pic"
-                            />
-                        }
-                            
-                              {authorTrend[2] && (<> {authorTrend[2].uname} </>)} </p>
-                        <p> {authorTrend[0] &&
-                        <img
-                                src={`data:image/jpeg;base64,${authorTrend[3].profileImage}`}
-                                onError={(e) => { e.target.onerror = null; e.target.src = 'https://www.pngall.com/wp-content/uploads/5/Profile.png' }}
-                                alt="User Profile Picture"
-                                className="profile-pic"
-                            />
-                        }  {authorTrend[3] && (<> {authorTrend[3].uname} </>)} </p>
-
-                    </div>
-                </div>
             </div>
         </>
     )
